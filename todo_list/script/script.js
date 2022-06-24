52124e6274a205ad5acdc1428c41509d63a3115d
Ernad Glavinic <ernadglavinic@Ernads-MacBook-Air.local>
2022-06-24 21:26:23 +0200

    /
    /

--- todo_list/script/script.js
+++ todo_list/script/script.js
@@ -1,8 +1,14 @@
 var todoContainer = document.querySelector(".display-container");
 var toDoItem = document.getElementById("to-do-input");
 
+var toDoArray = [];
+
 function loadToDos(container){
-    var data = JSON.parse(localStorage.getItem("todo"));
+    var data = JSON.parse(localStorage.getItem("ToDoItems"));
+
+    for(let i = 0; i <= data; i++){
+        toDoArray.push(data[i])
+    }
     console.log(data);
     container.innerHTML = "";
     
@@ -19,13 +25,17 @@ function loadToDos(container){
 }
 
 function insertToDo(){
-    localStorage.setItem("ToDoItem" + Math.round(Math.random() * 100), toDoItem.value);
-    toDoItem.value = "";
+        toDoArray = localStorage.getItem('ToDoItems');
+        toDoArray.push(toDoItem.value)
+        localStorage.setItem("ToDoItems", JSON.stringify(toDoArray));
+        toDoItem.value = "";
 }
-
+/**
 function deleteToDo(id){
-    todo.splice(id, 1);
-    loadToDos(todoContainer, todo);
-}
+    console.log(id);
+    toDoArray.splice(id, 1);
+    localStorage.setItem("ToDoItems", JSON.stringify(toDoArray));
+    loadToDos(todoContainer);
+} */
 
 loadToDos(todoContainer);
