14f21763a61c069fb515df5a71725e1d54c532c7
Ernad Glavinic <ernadglavinic@Ernads-MacBook-Air.local>
2022-06-28 22:46:22 +0200

    Adding JS
    I added funcionaliti for adding items in cart

--- shopping_cart/js/script.js
+++ shopping_cart/js/script.js
@@ -1,1 +1,31 @@
-alert("HEllo")
+var allTotal = 0;
+
+function addToCart(element){
+    let mainElement = element.closest('.single-item');
+    let name = mainElement.querySelector('h3').innerText;
+    let price = mainElement.querySelector('.price').innerText;
+    let quantity = parseInt(mainElement.querySelector('input').value);
+    let cartItems = document.querySelector('.cart-items');
+
+    if(quantity <= 0) {
+        alert("Quantity must be higher than 0!");
+        return;
+    }
+
+    price = price.substring(1);
+    price = parseFloat(price);
+
+    let total = price * quantity;
+
+    allTotal += total;
+
+    cartItems.innerHTML += `<div class="cart-single-item">
+                                <h3>${name}</h3>
+                                <p>$${price} * ${quantity} = $${total}</p>
+                            </div>`;
+
+    element.innerText = "Added";
+    element.setAttribute('disabled', 'true');
+
+    document.querySelector('.total').innerText = "Total amount: $" + allTotal;
+}
